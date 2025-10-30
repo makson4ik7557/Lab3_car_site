7cae4ed7cec260dea2751582fabe35c5abf61ab6
unknown <maksroomm@gmail.com>
2025-10-31 00:46:01 +0200

    Add car data and repository improvements

---
+++ push_changes.ps1
@@ -0,0 +1,8 @@
+Set-Location "D:\Lab3_car_site"
+git rm --cached commit_changes.bat commit_changes.ps1
+git add .
+git commit -m "Add car data and repository improvements"
+git pull origin main --rebase
+git push origin main
+Write-Host "Done" -ForegroundColor Green
+
