470c2e4e6cc70f03378c9312bd351dc02039348f
Maksym Martyn <135046406+makson4ik7557@users.noreply.github.com>
2025-10-31 01:46:44 +0200

    Delete git_push.ps1

--- git_push.ps1
+++
@@ -1,7 +0,0 @@
-Set-Location "D:\Lab3_car_site"
-git add .
-git commit -m "Update project with repository improvements"
-git pull origin main --rebase
-git push origin main
-Write-Host "Done" -ForegroundColor Green
-
