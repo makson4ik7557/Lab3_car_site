514e4c968f055c8cc968a53f2d341ccca138c492
Maksym Martyn <135046406+makson4ik7557@users.noreply.github.com>
2025-10-31 00:51:22 +0200

    Delete push_changes.ps1

--- push_changes.ps1
+++
@@ -1,8 +0,0 @@
-Set-Location "D:\Lab3_car_site"
-git rm --cached commit_changes.bat commit_changes.ps1 push_changes.ps1
-git add .
-git commit -m "Add car data and repository improvements"
-git pull origin main --rebase
-git push origin main
-Write-Host "Done" -ForegroundColor Green
-
